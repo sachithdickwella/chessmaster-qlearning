9ae22c8062a4b7d0e317e7ca9aa755eb68db5d16
Sachith Dickwella <sachith_prasanna@live.com>
2020-11-05 13:56:57 +0800

    Model mapping with the html

--- chessmaster-web/src/main/java/com/traviard/chessmaster/Main.java
+++ chessmaster-web/src/main/java/com/traviard/chessmaster/Main.java
@@ -1,7 +1,15 @@
 package com.traviard.chessmaster;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Sachith Dickwella
  */
+@SpringBootApplication
 public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
---
+++ chessmaster-web/src/main/java/com/traviard/chessmaster/controller/WebMvcController.java
@@ -0,0 +1,40 @@
+package com.traviard.chessmaster.controller;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Sachith Dickwella
+ */
+@PropertySource("classpath:app-config.properties")
+@Controller
+public class WebMvcController {
+
+    /**
+     * Application title in {@link String}.
+     */
+    @Value("${app.name}")
+    private String appName;
+    /**
+     * Application home page title in {@link String}.
+     */
+    @Value("${app.home.title}")
+    private String appHomeTitle;
+
+
+    /**
+     * Model mapping for the home/main page.
+     *
+     * @param model Instance of {@link Model}.
+     * @return the {@link String} of the page name.
+     */
+    @GetMapping(path = {"/", "index", "index.html"})
+    public String index(@NotNull Model model) {
+        model.addAttribute("title", appHomeTitle);
+        return "index";
+    }
+}
