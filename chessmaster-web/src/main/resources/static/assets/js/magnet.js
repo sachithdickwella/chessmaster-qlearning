74185c674df56f3a7c769748ca2d0d6fb9f18f3e
Sachith Dickwella <sachith_prasanna@live.com>
2020-11-16 13:39:00 +0800

    Adding drop piece event to drigger the image on model

--- chessmaster-web/src/main/resources/static/assets/js/magnet.js
+++ chessmaster-web/src/main/resources/static/assets/js/magnet.js
@@ -1,11 +1,11 @@
-let stompClient;
+let board;
 /**
  * Connect and subscribe a WebSocket. in order to receive server responses from
  * the model.
  */
 const connect = () => {
     const socket = new SockJS('/gs-guide-websocket');
-    stompClient = Stomp.over(socket);
+    const stompClient = Stomp.over(socket);
     stompClient.connect({}, _ => {
         const sessionId = socket._transport.url
             .match('(ws|wss):\\/\\/\\w+(?::[0-9]+)?\\/\\S+\\/[0-9]+\\/(\\w+)\\/websocket')[2]
@@ -20,8 +20,13 @@ const connect = () => {
  * @param next to give in the next move location on the board.
  */
 const nextMove = (next) => {
-    console.info(JSON.parse(next.body))
+    console.info(JSON.parse(next.body));
+    board.move(`${players.black.k0}-c6`);
 }
+/**
+ * The event of new move by the user.
+ */
+const onDrop = (source, target, piece, newPos, oldPos, orientation) => shot();
 /**
  * Shoot the chessboard HTML as a canvas and push it to the server end as an image.
  */
@@ -56,12 +61,38 @@ const push = (canvas) => {
     }, 'image/png');
 }
 /**
+ * Chess pieces. p# is Pawn, k# is Knight, r# is Rook, b# is bishop
+ * and King and Queen pieces respectively with the players colors.
+ */
+const players = {
+    white: {
+        p0: "a2", p1: "b2", p2: "c2", p3: "d2", p4: "e2", p5: "f2", p6: "g2", p7: "h2",
+        k0: "b1", k1: "g1",
+        r0: "a1", r1: "h1",
+        b0: "c1", b1: "f1",
+        king: "d1",
+        queen: "e1"
+    },
+    black: {
+        p0: "a7", p1: "b7", p2: "c7", p3: "d7", p4: "e7", p5: "f7", p6: "g7", p7: "h7",
+        k0: "b8", k1: "g8",
+        r0: "a8", r1: "h8",
+        b0: "c8", b1: "f8",
+        king: "d8",
+        queen: "e8"
+    }
+};
+/**
  * JQuery "ready" method to start the client side process and listeners.
  */
 $(() => {
-    const board = ChessBoard('board1', {
+    board = ChessBoard('board1', {
         draggable: true,
-        dropOffBoard: 'trash'
+        dropOffBoard: 'trash',
+        moveSpeed: 'slow',
+        snapbackSpeed: 500,
+        snapSpeed: 100,
+        onDrop: onDrop
     });
     board.start();
     /**
