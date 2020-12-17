7f2e7e784a69fe573c01af37b731ca16c4dca7c2
Sachith Dickwella <sachith_prasanna@live.com>
2020-12-17 17:09:27 +0800

    Implementing rule algorithm

--- chessmaster-core/src/util/algebraic_notation.py
+++ chessmaster-core/src/util/algebraic_notation.py
@@ -4,6 +4,16 @@ import re
 
 import numpy as np
 
+FLAGS = {
+    'n': 'a non capture',
+    'b': 'a pawn push of two squares',
+    'e': 'an en passant capture',
+    'c': 'a standard capture',
+    'p': 'a promotion',
+    'k': 'king-side castling',
+    'q': 'queen-side castling'
+}
+
 
 class Board(object):
 
@@ -17,11 +27,19 @@ class Board(object):
         self._board = self.init_board()
 
     def __getitem__(self, item):
+        """
+        Get the square value from the algebraic chess notation. If a chess piece is available
+        on that square, return the piece value. Otherwise 0 returns.
+
+        :param item: algebraic notation of the square (ex: e4, a8, b6).
+        :return: the square value for input algebraic notation.
+        """
         if type(item) is not str:
             raise TypeError('Item index should be Algebraic Notation')
-        elif not re.match('^[a-h][1-8]$', item):
+        elif not re.match('^[a-hA-H][1-8]$', item):
             raise KeyError('Item index doesn\'t match the pattern \'^[a-h][1-8]$\'')
         else:
+            item = item.lower()
             return self._board[self.ranks[int(item[1])], self.f_letters[item[0]]]
 
     def init_board(self):
@@ -36,12 +54,45 @@ class Board(object):
         return board
 
     def move(self, move):
+        """
+        Get the move details by passing the algebraic notation of the move and return
+        'None' of the move is an illegal.
+
+        :param move: algebraic notation of 2 squares that the piece should moved from
+        and the destination (ex: b2-b4).
+        :return: an instance of :func:`~Move` class with the details of source, target,
+        color, flag, target SAN and piece if it's legal move. Otherwise 'None' returns.
+        """
         if type(move) is str:
             raise TypeError('Item index should be Algebraic Notation')
-        elif not re.match('^([a-h][1-8]-?)+$', move):
+        elif not re.match('^([a-hA-H][1-8]-?)+$', move):
             raise KeyError('Item index doesn\'t match the pattern \'([a-h][1-8]-?)+$\'')
         else:
-            _from, _to = move.split('-')
+            _from, _to = move.lower().split('-')
             if self[_from] == 0:
                 return None
 
+
+class Move(object):
+
+    def __init__(self, color, flag, _from, _to, piece, san, captured=None):
+        super(Move, self).__init__()
+
+        self.color = color
+        self.flag = flag
+        self._from = _from
+        self._to = _to
+        self.piece = piece
+        self.san = san
+        self.captured = captured
+
+    def dict(self):
+        return {
+            'color': self.color,
+            'flag': self.flag,
+            'from': self._from,
+            'to': self._to,
+            'piece': self.piece,
+            'san': self.san,
+            'captured': self.captured
+        }
