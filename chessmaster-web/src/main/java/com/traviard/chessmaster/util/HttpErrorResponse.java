59ebc5781001c7485e13b5639c8493245343ae83
Sachith Dickwella <sachith_prasanna@live.com>
2021-03-19 11:22:18 +0800

    Migrate to Java 16 and update usecases of record and builder pattern

--- chessmaster-web/src/main/java/com/traviard/chessmaster/util/HttpErrorResponse.java
+++ chessmaster-web/src/main/java/com/traviard/chessmaster/util/HttpErrorResponse.java
@@ -1,39 +1,160 @@
 package com.traviard.chessmaster.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 /**
  * @author Sachith Dickwella
  */
-@Getter
-@Builder
+@SuppressWarnings("unused")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class HttpErrorResponse {
+public final class HttpErrorResponse {
 
     /**
      * Instance of {@link LocalDateTime} which error occurred.
      */
-    private final LocalDateTime timestamp;
+    private LocalDateTime timestamp;
     /**
      * HTTP status code for this error.
      */
-    private final int status;
+    private Integer status;
     /**
      * HTTP status code message as a {@link String}.
      */
-    private final String error;
+    private String error;
     /**
      * {@link String} message for the error instance.
      */
-    private final String message;
+    private String message;
     /**
      * Endpoint for request which failed.
      */
-    private final String path;
+    private String path;
+
+    /**
+     * Private constructor to avoid explicit object creation.
+     */
+    private HttpErrorResponse() {
+        // do nothing.
+    }
+
+    /**
+     * Initialize a new {@link Builder} instance and return.
+     *
+     * @return an instance of {@link Builder}.
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder pattern implementation class which initialize all of its super
+     * classes local members. In this case initialize local/instance members of
+     * {@link HttpErrorResponse} class.
+     */
+    public static class Builder {
+
+        /**
+         * Instance to refer super class members which should pattern be applied.
+         */
+        @JsonIgnore
+        private final HttpErrorResponse errorResponse;
+
+        /**
+         * Constructor to initialize {@link #errorResponse} instance during super
+         * class's {@link HttpErrorResponse#builder()} invoke.
+         */
+        private Builder() {
+            this.errorResponse = new HttpErrorResponse();
+        }
+
+        /**
+         * Initialize {@link HttpErrorResponse#timestamp} with argument value.
+         *
+         * @param timestamp of the object creation.
+         * @return this {@link Builder} instance to continue the build chain.
+         */
+        public Builder timestamp(LocalDateTime timestamp) {
+            errorResponse.timestamp = timestamp;
+            return this;
+        }
+
+        /**
+         * Initialize {@link HttpErrorResponse#status} with argument value.
+         *
+         * @param status of the error created.
+         * @return this {@link Builder} instance to continue the build chain.
+         */
+        public Builder status(Integer status) {
+            errorResponse.status = status;
+            return this;
+        }
+
+        /**
+         * Initialize {@link HttpErrorResponse#error} with argument value.
+         *
+         * @param error of the object entitled to.
+         * @return this {@link Builder} instance to continue the build chain.
+         */
+        public Builder error(String error) {
+            errorResponse.error = error;
+            return this;
+        }
+
+        /**
+         * Initialize {@link HttpErrorResponse#message} with argument value.
+         *
+         * @param message of the error object.
+         * @return this {@link Builder} instance to continue the build chain.
+         */
+        public Builder message(String message) {
+            errorResponse.message = message;
+            return this;
+        }
+
+        /**
+         * Initialize {@link HttpErrorResponse#path} with argument value.
+         *
+         * @param path of the request targeted.
+         * @return this {@link Builder} instance to continue the build chain.
+         */
+        public Builder path(String path) {
+            errorResponse.path = path;
+            return this;
+        }
+
+        /**
+         * Finalize the builder chain by returning the {@link #errorResponse}
+         * instance to the upstream.
+         *
+         * @return this instance of {@link #errorResponse} to upstream.
+         */
+        public HttpErrorResponse build() {
+            return errorResponse;
+        }
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
--- chessmaster-web/src/main/java/com/traviard/chessmaster/util/NextMove.java
+++ chessmaster-web/src/main/java/com/traviard/chessmaster/util/NextMove.java
@@ -2,34 +2,15 @@ package com.traviard.chessmaster.util;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
 
 import java.util.StringJoiner;
 
 /**
  * @author Sachith Dickwella
  */
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class NextMove {
-
-    /**
-     * UUID of the image originally send the frame to the model.
-     */
-    @SuppressWarnings("java:S116")
-    private String _id;
-    /**
-     * Target web socket session id which the model response should
-     * be sent up to.
-     */
-    @SuppressWarnings("java:S116")
-    private String _wsid;
-    /**
-     * Response from the model that states how the UI pieces should
-     * be moved.
-     */
-    private String move;
+public record NextMove(String _id, String _wsid, String move) {
 
     @Override
     public String toString() {
