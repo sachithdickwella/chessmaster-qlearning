618164168fd431070aa81141582d264cc07cbd8e
Sachith Dickwella <sachith_prasanna@live.com>
2022-07-06 17:49:32 +0800

    Update folder structure

--- chessmaster-core/model/qlearning.py
+++ chessmaster-core/model/qlearning.py
@@ -2,7 +2,6 @@
 
 from collections import namedtuple, OrderedDict
 
-import chess
 import numpy as np
 import torch
 import torch.nn as nn
@@ -15,17 +14,6 @@ TRANSITIONS = namedtuple('Transitions', ['piece', 'state', 'action', 'next_state
 
 PIECES = namedtuple('Pieces', ('PAWN', 'KNIGHT', 'BISHOP', 'ROOK', 'QUEEN', 'KING'))(*'pnbrqk')
 PIECES_ENCODING = list(dict(enumerate(PIECES)).keys())
-
-BOARD_SQUARES = [chess.A8, chess.B8, chess.C8, chess.D8, chess.E8, chess.F8, chess.G8, chess.H8,
-                 chess.A7, chess.B7, chess.C7, chess.D7, chess.E7, chess.F7, chess.G7, chess.H7,
-                 chess.A6, chess.B6, chess.C6, chess.D6, chess.E6, chess.F6, chess.G6, chess.H6,
-                 chess.A5, chess.B5, chess.C5, chess.D5, chess.E5, chess.F5, chess.G5, chess.H5,
-                 chess.A4, chess.B4, chess.C4, chess.D4, chess.E4, chess.F4, chess.G4, chess.H4,
-                 chess.A3, chess.B3, chess.C3, chess.D3, chess.E3, chess.F3, chess.G3, chess.H3,
-                 chess.A2, chess.B2, chess.C2, chess.D2, chess.E2, chess.F2, chess.G2, chess.H2,
-                 chess.A1, chess.B1, chess.C1, chess.D1, chess.E1, chess.F1, chess.G1, chess.H1]
-
-BOARD_SQUARES_ENCODING = list(dict(enumerate(BOARD_SQUARES)).keys())
 
 
 class ReplayMemory(object):
--- chessmaster-core/utils/__init__.py
+++ chessmaster-core/utils/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import logging.config
 import sys
 from logging.handlers import RotatingFileHandler
@@ -12,6 +13,18 @@ LOG_FILENAME = 'chessmaster-process.log'
 TEMP_PATH = f'{BUILD_PATH}/tmp'
 
 IMAGE_SIZE = (512, 512)
+
+if not os.path.exists(BUILD_PATH):
+    os.makedirs(BUILD_PATH)
+
+if not os.path.exists(IMAGE_PATH):
+    os.makedirs(IMAGE_PATH)
+
+if not os.path.exists(LOG_PATH):
+    os.makedirs(LOG_PATH)
+
+if not os.path.exists(TEMP_PATH):
+    os.makedirs(TEMP_PATH)
 
 # Logging configuration of the program.
 FORMAT = '%(asctime)s %(levelname)-8s  %(thread)-5d --- [%(threadName)s] %(filename)s : %(message)s'
